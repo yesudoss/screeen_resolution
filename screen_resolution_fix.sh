a28b35f3529071c96b3439cb5c0ac1f26fdc7c9e
Yesu Doss X <72723545+yesudoss@users.noreply.github.com>
2022-03-25 10:30:13 +0530

    Added screen resolution file

---
+++ screen_resolution_fix.sh
@@ -0,0 +1,4 @@
+cvt 1366 768 60.0
+xrandr --newmode "1368x768_60.00"   85.25  1368 1440 1576 1784  768 771 781 798 -hsync +vsync
+xrandr --addmode VGA-1 1368x768_60.00
+xrandr --output VGA-1 --mode 1368x768_60.00
